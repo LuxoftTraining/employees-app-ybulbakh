b99da455feb463334bdeb3a7141ab306229c703c
ybulbakh <ybulbakh>
2021-11-08 19:25:29 +0200

    Module 2 Lesson 2 Task 1

--- ui.js
+++ ui.js
@@ -4,21 +4,28 @@ let $ = (id) => {
     return document.getElementById(id)
 }
 
-runUi = (employees) => {
+let runUi = (employees) => {
     showEmployees(employees)
 }
 
-clearEmployeesPlaceholder = () => {
+let clearEmployeesPlaceholder = () => {
     document.getElementById(PLACEHOLDER).innerHTML = '';
 }
 
 showEmployees = (employees) => {
     clearEmployeesPlaceholder();
-    $(PLACEHOLDER).innerHTML = '<ul>'
+    const ul = document.createElement('ul')
     for (let employee of employees) {
-        $(PLACEHOLDER).innerHTML += '<li>' + employee.name + ' ' + employee.surname + '</li>'
+        const li = document.createElement('li')
+        ul.appendChild(li)
+        li.innerHTML = employee.name + " " + employee.surname;
+        const removeButton = document.createElement("button");
+        removeButton.innerHTML = "Delete";
+        removeButton.addEventListener('click',
+            () => removeEmployeeUI(employee.id));
+        li.appendChild(removeButton);
+        $(PLACEHOLDER).appendChild(ul)
     }
-    $(PLACEHOLDER).innerHTML += '</ul>'
 }
 
 function addEmployeeUI() {
@@ -26,8 +33,13 @@ function addEmployeeUI() {
     const surname = document.getElementById("surname").value;
     if (!name) $('addEmployeeFormErrorMessage').innerHTML += 'ERROR name<br>'
     if (!surname) $('addEmployeeFormErrorMessage').innerHTML += 'ERROR surname<br>'
-    const id = addEmployee(name, surname);
+    /*const id =*/ addEmployee(name, surname);
     document.getElementById("name").value = "";
     document.getElementById("surname").value = "";
     showEmployees(DATA.employees);
+}
+
+let removeEmployeeUI = (id) => {
+    removeEmployee(id)
+    showEmployees(DATA.employees)
 }
